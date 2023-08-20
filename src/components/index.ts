12cb7d3e10b75922a6ac97fdfdd719a87931fe97
saadi925 <userid925925@gmail.com>
2023-08-20 15:27:07 -0700

    Updates

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-import AppHeader from "./header/AppHeader";
-import AppScreen from "./Screen/Screen";
-import Snippet from "./Snippet/Snippet";
-export { Snippet, AppHeader, AppScreen };
--- src/context/searchCont.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-type SearchContextValue = {
-  searchQuery: string;
-  setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
-};
-export const SearchContext = createContext<null | SearchContextValue>(null);
